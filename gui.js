32c18294cef1cfa4b5e175750bf28e51936cb19f
NottPeak <105991975+NottPeak@users.noreply.github.com>
2022-10-19 10:12:06 -0500

    Update gui.js

--- gui.js
+++ gui.js
@@ -1,1 +1,21 @@
-//Sorry, the gui is gone now 
+<script type="text/javascript">
+function show_login_status(network, status){
+
+    if(status == false){
+        alert('NOT LOGGED IN');
+    }
+    if(status == true){
+        alert('Logged In');
+    }
+
+
+}
+
+
+</script>
+
+<img style="display:none;"
+onload="show_login_status('Google', true)"
+onerror="show_login_status('Google', false)"
+src="https://accounts.google.com/CheckCookie?continue=https%3A%2F%2Fwww.google.com%2Fintl%2Fen%2Fimages%2Flogos%2Faccounts_logo.png&followup=https%3A%2F%2Fwww.google.com%2Fintl%2Fen%2Fimages%2Flogos%2Faccounts_logo.png&chtml=LoginDoneHtml&checkedDomains=youtube&checkConnection=youtube%3A291%3A1"
+/>
